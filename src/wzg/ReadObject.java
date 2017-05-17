3c00e03d8c632571d59839032e24b61087a17816
Wzg <17826826757@163.com>
2017-05-17 22:27:57 +0800

    增加注释

--- src/wzg/ReadObject.java
+++ src/wzg/ReadObject.java
@@ -6,8 +6,10 @@ import java.io.ObjectInputStream;
 public class ReadObject {
 
 	public static void main(String[] args) {
-		try(ObjectInputStream ois=new ObjectInputStream(new FileInputStream("object.txt")))
+		try(//创建一个ObjectInputStream输入流
+				ObjectInputStream ois=new ObjectInputStream(new FileInputStream("object.txt")))
 		{
+			//从输入流读取一个Java对象，并将其强制类型转换为Person类
 			Person person=(Person)ois.readObject();
 			System.out.println("名字为："+person.getName()+"\n年龄为："+person.getAge()+"岁");
 		} catch (Exception e) {
--- src/wzg/WriterObject.java
+++ src/wzg/WriterObject.java
@@ -7,9 +7,11 @@ public class WriterObject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//创建一个ObjectOutputStream输出流
 		try(ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream("object.txt")))
 		{
 			Person person = new Person("蛋蛋", 23);
+			//将对象写入输出流
 			oos.writeObject(person);
 		} catch (Exception e) {
 			e.printStackTrace();
